9d1b5dac762d24b8097bfbafdcb96b8dbcc60f40
Jomalang <rhkddlr98@naver.com>
2024-07-30 17:20:26 +0900

    스프링부트 환경설정 완료

---
+++ src/test/java/Recorders/ggogit/GgogitApplicationTests.java
@@ -0,0 +1,13 @@
+package Recorders.ggogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

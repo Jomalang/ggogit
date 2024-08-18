8e8222f95505346f3a07aa44d30e3242e392912f
Gun-code <dlgmlrnjs213@gmail.com>
2024-08-19 01:09:03 +0900

    Add: user-tree-index 레이아웃및 프레그먼트 생성

--- src/main/java/Recorders/ggogit/controller/tree/UserController.java
+++ src/main/java/Recorders/ggogit/controller/tree/UserController.java
@@ -25,13 +25,10 @@ public class UserController {
     }
     @GetMapping("/tree/memoir/index")
     public String getTreeMemoir(Model model) {
-        List<Branch> lists2 = new ArrayList<>();
-        lists2.add(new Branch("heegwon-branch","card-bookmark-icon.svg","2024-07-18","브랜치 이름",999,90));
-        lists2.add(new Branch("taegyu-branch","card-bookmark-icon.svg","2024-07-18","브랜치 이름",999,90));
-        lists2.add(new Branch("hyeonjin-branch","card-tree-icon.svg","2024-07-18","브랜치 이름",999,90));
-        lists2.add(new Branch("jinpeal-branch","card-bookmark-icon.svg","2024-07-18","브랜치 이름",999,90));
-        lists2.add(new Branch("jaeyoung-branch","card-bookmark-icon.svg","2024-07-18","브랜치 이름",999,90));
-        model.addAttribute("lists", lists2);
+
+        model.addAttribute("type1", new int[]{1, 1, 1});
+        model.addAttribute("type2", new int[]{2, 2, 2});
+        model.addAttribute("type3", new int[]{3, 3, 3});
         return "view/user/tree/memoir/index";
     }
 }

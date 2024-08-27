1d53d8f696b636f17cb8fd7ce287cb926481d154
TaegyuHan <gksxorb147@gmail.com>
2024-08-27 18:34:04 +0900

    Add: #46 데이터 적재 진행중 - 리프 맵핑 데이터 진행중

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -15,26 +15,36 @@
 -- 모든 테이블 제거
 -- ============================================ --
 BEGIN
+    -- 테이블 제거
     FOR i IN (SELECT table_name FROM user_tables) LOOP
         EXECUTE IMMEDIATE 'DROP TABLE ' || i.table_name || ' CASCADE CONSTRAINTS';
     END LOOP;
+
+    -- 시퀀스 제거
+    FOR i IN (SELECT sequence_name FROM user_sequences) LOOP
+        EXECUTE IMMEDIATE 'DROP SEQUENCE ' || i.sequence_name;
+    END LOOP;
+
+    COMMIT;
 END;
+
+-- 시퀀스 제거
+PURGE RECYCLEBIN;
 
 -- ============================================ --
 -- 회원 테이블
 -- ============================================ --
 CREATE TABLE "MEMBER" (
-    "ID"	            NUMBER		            NOT NULL, -- 회원 PK
+    "ID"	            NUMBER		            PRIMARY KEY, -- 회원 PK
     "EMAIL"	            VARCHAR2(255)	        NOT NULL, -- 회원 이메일
     "PASSWORD"	        VARCHAR2(64)	        NOT NULL, -- 회원 비밀번호
     "NICKNAME"	        VARCHAR2(255)		    NOT NULL, -- 회원 닉네임
     "INTRODUCTION"	    NVARCHAR2(2000) 	    NULL, -- 회원 소개글
     "UPDATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 수정 시각
-    "CREATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 생성 시각
-    -- PK 정의
-    CONSTRAINT "PK_MEMBER" PRIMARY KEY ("ID")
+    "CREATE_TIME"	    TIMESTAMP		        NOT NULL -- 데이터 생성 시각
 );
 
+-- 코맨트
 COMMENT ON COLUMN "MEMBER"."ID" IS '회원 PK';
 COMMENT ON COLUMN "MEMBER"."EMAIL" IS '회원 이메일';
 COMMENT ON COLUMN "MEMBER"."PASSWORD" IS '회원 비밀번호';
@@ -50,7 +60,9 @@ CREATE TABLE "MEMBER_BACKGROUND_IMAGE" (
     "ID"	        NUMBER		    NOT NULL, -- 회원 배경 이미지 PK
     "NAME"	        VARCHAR2(255)	NOT NULL, -- 회원 배경 이미지 이름
     -- PK 정의
-    CONSTRAINT "PK_MEMBER_BACKGROUND_IMAGE" PRIMARY KEY ("ID")
+    CONSTRAINT "PK_MEMBER_BACKGROUND_IMAGE" PRIMARY KEY ("ID"),
+    -- FK 정의
+    CONSTRAINT "FK_MEMBER_BACKGROUND_IMAGE_MEMBER" FOREIGN KEY ("ID") REFERENCES "MEMBER" ("ID")
 );
 
 COMMENT ON COLUMN "MEMBER_BACKGROUND_IMAGE"."ID" IS '회원 배경 이미지 PK';
@@ -80,7 +92,7 @@ COMMENT ON COLUMN "FOLLOW"."CREATE_TIME" IS '데이터 생성 시각';
 -- 도서 테이블
 -- ============================================ --
 CREATE TABLE "BOOK" (
-    "ID"             NUMBER		            NOT NULL, -- 도서 PK
+    "ID"	         NUMBER		            PRIMARY KEY, -- 도서 PK
     "MEMBER_ID"      NUMBER		            NOT NULL, -- 회원 FK
     "NAME"           NVARCHAR2(1024)	    NOT NULL, -- 도서 제목
     "AUTHOR"         NVARCHAR2(1024)	    NOT NULL, -- 도서 저자
@@ -89,9 +101,7 @@ CREATE TABLE "BOOK" (
     "IMAGE_FILE"     VARCHAR2(1024)	        NULL, -- 도서 이미지 파일
     "UPDATE_TIME"    TIMESTAMP	            NOT NULL, -- 데이터 수정 시각
     "CREATE_TIME"    TIMESTAMP	            NOT NULL, -- 데이터 생성 시각
-    "RESOURCE_FROM"  NUMBER(1)	            NOT NULL, -- 도서 출처
-    -- PK 정의
-    CONSTRAINT "PK_BOOK" PRIMARY KEY ("ID"),
+    "RESOURCE_FROM"  NUMBER(1)	            NOT NULL, -- 도서 출처(0: 직접입력, 1: API 입력)
     -- FK 정의
     CONSTRAINT "FK_BOOK_MEMBER" FOREIGN KEY ("MEMBER_ID") REFERENCES "MEMBER" ("ID")
 );
@@ -105,7 +115,7 @@ COMMENT ON COLUMN "BOOK"."TOTAL_PAGE" IS '도서 총 페이지 수';
 COMMENT ON COLUMN "BOOK"."IMAGE_FILE" IS '도서 이미지 파일';
 COMMENT ON COLUMN "BOOK"."UPDATE_TIME" IS '데이터 수정 시각';
 COMMENT ON COLUMN "BOOK"."CREATE_TIME" IS '데이터 생성 시각';
-COMMENT ON COLUMN "BOOK"."RESOURCE_FROM" IS '도서 출처';
+COMMENT ON COLUMN "BOOK"."RESOURCE_FROM" IS '도서 출처 (0: 직접입력, 1: API 입력)';
 
 -- ============================================ --
 -- 도서 좋아요 테이블
@@ -137,15 +147,13 @@ COMMENT ON COLUMN "BOOK_LIKE"."CREATE_TIME" IS '데이터 생성 시각';
 -- 도서 댓글 테이블
 -- ============================================ --
 CREATE TABLE "BOOK_COMMENT" (
-    "ID"	        NUMBER		                    NOT NULL, -- 도서 댓글 PK
+    "ID"	        NUMBER		                    PRIMARY KEY, -- 도서 댓글 PK
     "MEMBER_ID"	    NUMBER		                    NOT NULL, -- 회원 FK
     "BOOK_ID"	    NUMBER		                    NOT NULL, -- 도서 FK
     "LIKE_COUNT"	NUMBER	            DEFAULT 0	NOT NULL, -- 도서 댓글 좋아요 수
     "COMMENT"	    NVARCHAR2(2000)		            NOT NULL, -- 도서 댓글 내용
     "UPDATE_TIME"	TIMESTAMP		                NOT NULL, -- 데이터 수정 시각
     "CREATE_TIME"	TIMESTAMP		                NOT NULL, -- 데이터 생성 시각
-    -- PK 정의
-    CONSTRAINT "PK_BOOK_COMMENT" PRIMARY KEY ("ID"),
     -- FK 정의
     CONSTRAINT "FK_BOOK_COMMENT_MEMBER" FOREIGN KEY ("MEMBER_ID") REFERENCES "MEMBER" ("ID"),
     CONSTRAINT "FK_BOOK_COMMENT_BOOK" FOREIGN KEY ("BOOK_ID") REFERENCES "BOOK" ("ID")
@@ -190,23 +198,24 @@ COMMENT ON COLUMN "BOOK_COMMENT_LIKE"."UPDATE_TIME" IS '데이터 수정 시각'
 COMMENT ON COLUMN "BOOK_COMMENT_LIKE"."CREATE_TIME" IS '데이터 생성 시각';
 
 -- ============================================ --
--- 씨드 테이블
+-- 씨앗 테이블
 -- ============================================ --
 CREATE TABLE "SEED" (
-    "ID"	            NUMBER		        NOT NULL, -- 씨앗 PK
+    "ID"	            NUMBER		        PRIMARY KEY, -- 씨앗 PK
     "NAME"	            NVARCHAR2(30)		NOT NULL, -- 씨앗 제목
-    -- PK 정의
-    CONSTRAINT "PK_SEED" PRIMARY KEY ("ID")
+    "DESCRIPTION"	    NVARCHAR2(30)		NOT NULL -- 씨앗 설명
 );
 
 COMMENT ON COLUMN "SEED"."ID" IS '씨앗 PK';
 COMMENT ON COLUMN "SEED"."NAME" IS '씨앗 제목';
+COMMENT ON COLUMN "SEED"."DESCRIPTION" IS '씨앗 설명';
 
 -- ============================================ --
 -- 트리 테이블
 -- ============================================ --
 CREATE TABLE "TREE" (
-    "ID"	            NUMBER		        NOT NULL,
+    "ID"	            NUMBER		        PRIMARY KEY,
+    "BOOK_ID"	        NUMBER		        NULL, -- NULL 가능
     "MEMBER_ID"	        NUMBER		        NOT NULL,
     "SEED_ID"	        NUMBER		        NOT NULL,
     "TITLE"	            NVARCHAR2(300)		NOT NULL,
@@ -215,9 +224,8 @@ CREATE TABLE "TREE" (
 	"VISIBILITY"	    NUMBER(1)	        DEFAULT 1	NOT NULL,
 	"UPDATE_TIME"	    TIMESTAMP           DEFAULT CURRENT_TIMESTAMP	NOT NULL,
 	"CREATE_TIME"	    TIMESTAMP	        DEFAULT CURRENT_TIMESTAMP	NOT NULL,
-    -- PK 정의
-    CONSTRAINT "PK_TREE" PRIMARY KEY ("ID"),
     -- FK 정의
+    CONSTRAINT "FK_TREE_BOOK" FOREIGN KEY ("BOOK_ID") REFERENCES "BOOK" ("ID"),
     CONSTRAINT "FK_TREE_MEMBER" FOREIGN KEY ("MEMBER_ID") REFERENCES "MEMBER" ("ID"),
     CONSTRAINT "FK_TREE_SEED" FOREIGN KEY ("SEED_ID") REFERENCES "SEED" ("ID")
 );
@@ -227,6 +235,7 @@ CREATE INDEX "IDX_TREE_MEMBER_ID" ON "TREE" ("MEMBER_ID");
 CREATE INDEX "IDX_TREE_SEED_ID" ON "TREE" ("SEED_ID");
 
 COMMENT ON COLUMN "TREE"."ID" IS '트리 PK';
+COMMENT ON COLUMN "TREE"."BOOK_ID" IS '도서 FK';
 COMMENT ON COLUMN "TREE"."MEMBER_ID" IS '회원 FK';
 COMMENT ON COLUMN "TREE"."SEED_ID" IS '씨앗 고유 FK';
 COMMENT ON COLUMN "TREE"."TITLE" IS '트리 제목';
@@ -240,12 +249,14 @@ COMMENT ON COLUMN "TREE"."CREATE_TIME" IS '데이터 생성 시각';
 -- 트리 책 추가 정보 테이블
 -- ============================================ --
 CREATE TABLE "TREE_BOOK" (
-    "ID"	        NUMBER		    NOT NULL, -- 책 트리 PK
+    "ID"	        NUMBER		    NOT NULL, -- 트리 FK
     "TOTAL_PAGE"	NUMBER(5)		NOT NULL, -- 책 트리 총 페이지 수
     "READING_PAGE"	NUMBER(5)		NOT NULL, -- 책 트리 읽은 페이지 수
     "PROGRESS"	    NUMBER(3)		NOT NULL, -- 책 트리 진행률
     -- PK 정의
-    CONSTRAINT "PK_TREE_BOOK" PRIMARY KEY ("ID")
+    CONSTRAINT "PK_TREE_BOOK" PRIMARY KEY ("ID"),
+    -- FK 정의
+    CONSTRAINT "FK_TREE_BOOK_TREE" FOREIGN KEY ("ID") REFERENCES "TREE" ("ID")
 );
 
 COMMENT ON COLUMN "TREE_BOOK"."ID" IS '책 트리 PK';
@@ -257,10 +268,12 @@ COMMENT ON COLUMN "TREE_BOOK"."PROGRESS" IS '책 트리 진행률';
 -- 트리 이미지 테이블
 -- ============================================ --
 CREATE TABLE "TREE_IMAGE" (
-    "ID"	NUMBER		    NOT NULL, -- 트리 이미지 PK
+    "ID"	NUMBER		    NOT NULL, -- 트리 FK
     "NAME"	VARCHAR2(255)	NOT NULL, -- 트리 이미지 이름
-    --- PK 정의
-    CONSTRAINT "PK_TREE_IMAGE" PRIMARY KEY ("ID")
+    -- PK 정의
+    CONSTRAINT "PK_TREE_IMAGE" PRIMARY KEY ("ID"),
+    -- FK 정의
+    CONSTRAINT "FK_TREE_IMAGE_TREE" FOREIGN KEY ("ID") REFERENCES "TREE" ("ID")
 );
 
 COMMENT ON COLUMN "TREE_IMAGE"."ID" IS '트리 이미지 PK';
@@ -296,17 +309,15 @@ COMMENT ON COLUMN "TREE_LIKE"."CREATE_TIME" IS '데이터 생성 시각';
 -- 회고록 테이블
 -- ============================================ --
 CREATE TABLE "MEMOIR" (
-                          "ID"	            NUMBER		            NOT NULL, -- 회고록 PK
-                          "TREE_ID"	        NUMBER		            NOT NULL, -- 트리 FK
-                          "TITLE"	            VARCHAR(255)		    NOT NULL, -- 회고록 제목
-                          "TEXT"	            CLOB		            NOT NULL, -- 회고록 내용
-                          "VISIBILITY"	    NUMBER(1)	DEFAULT 1	NOT NULL, -- 사용자 공개 여부
-                          "UPDATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 수정 시각
-                          "CREATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 생성 시각
-    -- PK 정의
-                          CONSTRAINT "PK_MEMOIR" PRIMARY KEY ("ID"),
+    "ID"	            NUMBER		            PRIMARY KEY, -- 회고록 PK
+    "TREE_ID"	        NUMBER		            NOT NULL, -- 트리 FK
+    "TITLE"	            VARCHAR(255)		    NOT NULL, -- 회고록 제목
+    "TEXT"	            CLOB		            NOT NULL, -- 회고록 내용
+    "VISIBILITY"	    NUMBER(1)	DEFAULT 1	NOT NULL, -- 사용자 공개 여부
+    "UPDATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 수정 시각
+    "CREATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 생성 시각
     -- FK 정의
-                          CONSTRAINT "FK_MEMOIR_TREE" FOREIGN KEY ("TREE_ID") REFERENCES "TREE" ("ID")
+    CONSTRAINT "FK_MEMOIR_TREE" FOREIGN KEY ("TREE_ID") REFERENCES "TREE" ("ID")
 );
 
 -- 인덱스 정의
@@ -324,16 +335,16 @@ COMMENT ON COLUMN "MEMOIR"."CREATE_TIME" IS '데이터 생성 시각';
 -- 회고록 좋아요 테이블
 -- ============================================ --
 CREATE TABLE "MEMOIR_LIKE" (
-                               "MEMBER_ID"	    NUMBER		            NOT NULL, -- 회원 FK
-                               "MEMOIR_ID"	    NUMBER		            NOT NULL, -- 회고록 FK
-                               "ACTIVATE"	    NUMBER(1)	DEFAULT 1	NOT NULL, -- 회고록 좋아요 활성화 여부
-                               "UPDATE_TIME"	TIMESTAMP		        NOT NULL, -- 데이터 수정 시각
-                               "CREATE_TIME"	TIMESTAMP		        NOT NULL, -- 데이터 생성 시각
+    "MEMBER_ID"	    NUMBER		            NOT NULL, -- 회원 FK
+    "MEMOIR_ID"	    NUMBER		            NOT NULL, -- 회고록 FK
+    "ACTIVATE"	    NUMBER(1)	DEFAULT 1	NOT NULL, -- 회고록 좋아요 활성화 여부
+    "UPDATE_TIME"	TIMESTAMP		        NOT NULL, -- 데이터 수정 시각
+    "CREATE_TIME"	TIMESTAMP		        NOT NULL, -- 데이터 생성 시각
     -- PK 정의
-                               CONSTRAINT "PK_MEMOIR_LIKE" PRIMARY KEY ("MEMBER_ID", "MEMOIR_ID"),
+    CONSTRAINT "PK_MEMOIR_LIKE" PRIMARY KEY ("MEMBER_ID", "MEMOIR_ID"),
     -- FK 정의
-                               CONSTRAINT "FK_MEMOIR_LIKE_MEMBER" FOREIGN KEY ("MEMBER_ID") REFERENCES "MEMBER" ("ID"),
-                               CONSTRAINT "FK_MEMOIR_LIKE_MEMOIR" FOREIGN KEY ("MEMOIR_ID") REFERENCES "MEMOIR" ("ID")
+    CONSTRAINT "FK_MEMOIR_LIKE_MEMBER" FOREIGN KEY ("MEMBER_ID") REFERENCES "MEMBER" ("ID"),
+    CONSTRAINT "FK_MEMOIR_LIKE_MEMOIR" FOREIGN KEY ("MEMOIR_ID") REFERENCES "MEMOIR" ("ID")
 );
 
 COMMENT ON COLUMN "MEMOIR_LIKE"."MEMBER_ID" IS '회원 FK';
@@ -346,18 +357,16 @@ COMMENT ON COLUMN "MEMOIR_LIKE"."CREATE_TIME" IS '데이터 생성 시각';
 -- 회고록 댓글 테이블
 -- ============================================ --
 CREATE TABLE "MEMOIR_COMMENT" (
-                                  "ID"	            NUMBER		            NOT NULL, -- 회고록 댓글 PK
-                                  "MEMBER_ID"	        NUMBER		            NOT NULL, -- 회원 FK
-                                  "MEMOIR_ID"	        NUMBER		            NOT NULL, -- 회고록 FK
-                                  "LIKE_COUNT"	    NUMBER	DEFAULT 0	NOT NULL, -- 회고록 댓글 좋아요 수
-                                  "COMMENT"	        NVARCHAR2(2000)		    NOT NULL, -- 회고록 댓글 내용
-                                  "UPDATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 수정 시각
-                                  "CREATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 생성 시각
-    -- PK 정의
-                                  CONSTRAINT "PK_MEMOIR_COMMENT" PRIMARY KEY ("ID"),
+    "ID"	            NUMBER		            PRIMARY KEY, -- 회고록 댓글 PK
+    "MEMBER_ID"	        NUMBER		            NOT NULL, -- 회원 FK
+    "MEMOIR_ID"	        NUMBER		            NOT NULL, -- 회고록 FK
+    "LIKE_COUNT"	    NUMBER	DEFAULT 0	    NOT NULL, -- 회고록 댓글 좋아요 수
+    "COMMENT"	        NVARCHAR2(2000)		    NOT NULL, -- 회고록 댓글 내용
+    "UPDATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 수정 시각
+    "CREATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 생성 시각
     -- FK 정의
-                                  CONSTRAINT "FK_MEMOIR_COMMENT_MEMBER" FOREIGN KEY ("MEMBER_ID") REFERENCES "MEMBER" ("ID"),
-                                  CONSTRAINT "FK_MEMOIR_COMMENT_MEMOIR" FOREIGN KEY ("MEMOIR_ID") REFERENCES "MEMOIR" ("ID")
+    CONSTRAINT "FK_MEMOIR_COMMENT_MEMBER" FOREIGN KEY ("MEMBER_ID") REFERENCES "MEMBER" ("ID"),
+    CONSTRAINT "FK_MEMOIR_COMMENT_MEMOIR" FOREIGN KEY ("MEMOIR_ID") REFERENCES "MEMOIR" ("ID")
 );
 
 -- 인덱스 정의
@@ -376,16 +385,16 @@ COMMENT ON COLUMN "MEMOIR_COMMENT"."CREATE_TIME" IS '데이터 생성 시각';
 -- 회고록 댓글 좋아요 테이블
 -- ============================================ --
 CREATE TABLE "MEMOIR_COMMENT_LIKE" (
-                                       "MEMBER_ID"	        NUMBER		            NOT NULL, -- 회원 FK
-                                       "MEMOIR_COMMENT_ID"	NUMBER		            NOT NULL, -- 회고록 댓글 FK
-                                       "ACTIVATE"	        NUMBER(1)	DEFAULT 1	NOT NULL, -- 회고록 댓글 좋아요 활성화 여부
-                                       "UPDATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 수정 시각
-                                       "CREATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 생성 시각
+    "MEMBER_ID"	        NUMBER		            NOT NULL, -- 회원 FK
+    "MEMOIR_COMMENT_ID"	NUMBER		            NOT NULL, -- 회고록 댓글 FK
+    "ACTIVATE"	        NUMBER(1)	DEFAULT 1	NOT NULL, -- 회고록 댓글 좋아요 활성화 여부
+    "UPDATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 수정 시각
+    "CREATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 생성 시각
     -- PK 정의
-                                       CONSTRAINT "PK_MEMOIR_COMMENT_LIKE" PRIMARY KEY ("MEMBER_ID", "MEMOIR_COMMENT_ID"),
+    CONSTRAINT "PK_MEMOIR_COMMENT_LIKE" PRIMARY KEY ("MEMBER_ID", "MEMOIR_COMMENT_ID"),
     -- FK 정의
-                                       CONSTRAINT "FK_MEMOIR_COMMENT_LIKE_MEMBER" FOREIGN KEY ("MEMBER_ID") REFERENCES "MEMBER" ("ID"),
-                                       CONSTRAINT "FK_MEMOIR_COMMENT_LIKE_MEMOIR_COMMENT" FOREIGN KEY ("MEMOIR_COMMENT_ID") REFERENCES "MEMOIR_COMMENT" ("ID")
+    CONSTRAINT "FK_MEMOIR_COMMENT_LIKE_MEMBER" FOREIGN KEY ("MEMBER_ID") REFERENCES "MEMBER" ("ID"),
+    CONSTRAINT "FK_MEMOIR_COMMENT_LIKE_MEMOIR_COMMENT" FOREIGN KEY ("MEMOIR_COMMENT_ID") REFERENCES "MEMOIR_COMMENT" ("ID")
 );
 
 COMMENT ON COLUMN "MEMOIR_COMMENT_LIKE"."MEMBER_ID" IS '회원 FK';
@@ -398,13 +407,11 @@ COMMENT ON COLUMN "MEMOIR_COMMENT_LIKE"."CREATE_TIME" IS '데이터 생성 시�
 -- 리프 태그 테이블
 -- ============================================ --
 CREATE TABLE "LEAF_TAG" (
-    "ID"	        NUMBER		    NOT NULL, -- 리프 태그 PK
+    "ID"	        NUMBER		    PRIMARY KEY, -- 리프 태그 PK
     "MEMBER_ID"	    NUMBER		    NOT NULL, -- 회원 FK
     "NAME"	        NVARCHAR2(20)	NOT NULL, -- 리프 태그 이름
     "UPDATE_TIME"	TIMESTAMP		NOT NULL, -- 데이터 수정 시각
     "CREATE_TIME"	TIMESTAMP 		NOT NULL, -- 데이터 생성 시각
-    -- PK 정의
-    CONSTRAINT "PK_LEAF_TAG" PRIMARY KEY ("ID"),
     -- FK 정의
     CONSTRAINT "FK_LEAF_TAG_MEMBER" FOREIGN KEY ("MEMBER_ID") REFERENCES "MEMBER" ("ID")
 );
@@ -422,7 +429,7 @@ COMMENT ON COLUMN "LEAF_TAG"."CREATE_TIME" IS '데이터 생성 시각';
 -- 리프 테이블
 -- ============================================ --
 CREATE TABLE "LEAF" (
-    "ID"	                NUMBER		                        NOT NULL,
+    "ID"	                NUMBER		                        PRIMARY KEY,
     "TREE_ID"	            NUMBER		                        NOT NULL,
     "PARENT_LEAF_ID"	    NUMBER		                        NULL,
     "VISIBILITY"	        NUMBER(1)	    DEFAULT 1	        NOT NULL,
@@ -433,9 +440,7 @@ CREATE TABLE "LEAF" (
 	"CHILD_LEAF_COUNT"	    NUMBER(1)	    DEFAULT 0	        NOT NULL,
 	"UPDATE_TIME"	        TIMESTAMP		                    NOT NULL,
 	"CREATE_TIME"	        TIMESTAMP		                    NOT NULL,
-	"BOOK_MARK"	            NUMBER(1)	    DEFAULT 0	        NOT NULL,
-    -- PK 정의
-    CONSTRAINT "PK_LEAF" PRIMARY KEY ("ID"),
+	"BOOK_MARK_COUNT"	    NUMBER(1)	    DEFAULT 0	        NOT NULL,
     -- FK 정의
     CONSTRAINT "FK_LEAF_TREE" FOREIGN KEY ("TREE_ID") REFERENCES "TREE" ("ID"),
     CONSTRAINT "FK_LEAF_PARENT_LEAF" FOREIGN KEY ("PARENT_LEAF_ID") REFERENCES "LEAF" ("ID")
@@ -457,13 +462,13 @@ COMMENT ON COLUMN "LEAF"."CREATE_TIME" IS '데이터 생성 시각';
 -- 리프 태그 맵 테이블
 -- ============================================ --
 CREATE TABLE "LEAF_TAG_MAP" (
-                                "LEAF_ID"	    NUMBER		    NOT NULL, -- 리프 FK
-                                "LEAF_TAG_ID"	NUMBER		    NOT NULL, -- 리프 태그 FK
+    "LEAF_ID"	    NUMBER		    NOT NULL, -- 리프 FK
+    "LEAF_TAG_ID"	NUMBER		    NOT NULL, -- 리프 태그 FK
     -- PK 정의
-                                CONSTRAINT "PK_LEAF_TAG_MAP" PRIMARY KEY ("LEAF_ID", "LEAF_TAG_ID"),
+    CONSTRAINT "PK_LEAF_TAG_MAP" PRIMARY KEY ("LEAF_ID", "LEAF_TAG_ID"),
     -- FK 정의
-                                CONSTRAINT "FK_LEAF_TAG_MAP_LEAF" FOREIGN KEY ("LEAF_ID") REFERENCES "LEAF" ("ID"),
-                                CONSTRAINT "FK_LEAF_TAG_MAP_LEAF_TAG" FOREIGN KEY ("LEAF_TAG_ID") REFERENCES "LEAF_TAG" ("ID")
+    CONSTRAINT "FK_LEAF_TAG_MAP_LEAF" FOREIGN KEY ("LEAF_ID") REFERENCES "LEAF" ("ID"),
+    CONSTRAINT "FK_LEAF_TAG_MAP_LEAF_TAG" FOREIGN KEY ("LEAF_TAG_ID") REFERENCES "LEAF_TAG" ("ID")
 );
 
 COMMENT ON COLUMN "LEAF_TAG_MAP"."LEAF_ID" IS '리프 FK';
@@ -515,15 +520,13 @@ COMMENT ON COLUMN "LEAF_LIKE"."CREATE_TIME" IS '데이터 생성 시각';
 -- 리프 댓글 테이블
 -- ============================================ --
 CREATE TABLE "LEAF_COMMENT" (
-    "ID"	            NUMBER		            NOT NULL, -- 리프 댓글 PK
+    "ID"	            NUMBER		            PRIMARY KEY, -- 리프 댓글 PK
     "MEMBER_ID"	        NUMBER		            NOT NULL, -- 회원 FK
     "LEAF_ID"	        NUMBER		            NOT NULL, -- 리프 FK
-    "LIKE_COUNT"	    NUMBER	DEFAULT 0	NOT NULL, -- 리프 댓글 좋아요 수
+    "LIKE_COUNT"	    NUMBER	DEFAULT 0	    NOT NULL, -- 리프 댓글 좋아요 수
     "COMMENT"	        NVARCHAR2(2000)		    NOT NULL, -- 리프 댓글 내용
     "UPDATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 수정 시각
     "CREATE_TIME"	    TIMESTAMP		        NOT NULL, -- 데이터 생성 시각
-    -- PK 정의
-    CONSTRAINT "PK_LEAF_COMMENT" PRIMARY KEY ("ID"),
     -- FK 정의
     CONSTRAINT "FK_LEAF_COMMENT_MEMBER" FOREIGN KEY ("MEMBER_ID") REFERENCES "MEMBER" ("ID"),
     CONSTRAINT "FK_LEAF_COMMENT_LEAF" FOREIGN KEY ("LEAF_ID") REFERENCES "LEAF" ("ID")

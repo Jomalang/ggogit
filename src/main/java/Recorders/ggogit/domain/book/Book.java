9add5f5d834231fbfc96e184511a47f7f2757744
pilpearl <playinyouth@gmail.com>
2024-08-29 14:50:29 +0900

    Book Entity 완료

---
+++ src/main/java/Recorders/ggogit/domain/book/Book.java
@@ -0,0 +1,23 @@
+package Recorders.ggogit.domain.book;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+    private Long id;
+    private Long memberId;
+    private Long totalPage;
+    private String title;
+    private String author;
+    private String publisher;
+    private String imageFile;
+    private Boolean resourceFrom;
+    private LocalDate publicDate;
+    private LocalDate updateTime;
+    private LocalDate createTime;
+}

a3d4df34d1a424647ac6a0698646515da90fc726
JJ <jyj961004@gmail.com>
2024-08-29 22:23:57 +0900

    Add:member-entity,repository,mapper

--- src/main/java/Recorders/ggogit/domain/member/repository/MemberBackgroundImageRepository.java
+++ src/main/java/Recorders/ggogit/domain/member/repository/MemberBackgroundImageRepository.java
@@ -3,7 +3,21 @@ package Recorders.ggogit.domain.member.repository;
 import Recorders.ggogit.domain.member.entity.MemberBackgroundImage;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface MemberBackgroundImageRepository {
+
     void save(MemberBackgroundImage memberBackgroundImage);
+
+    void update(MemberBackgroundImage memberBackgroundImage);
+
+    void delete(MemberBackgroundImage memberBackgroundImage);
+
+    List<MemberBackgroundImage> findAll();
+
+    MemberBackgroundImage findById(Long id);
+
+    MemberBackgroundImage findByName(String name);
+
 }
--- src/main/java/Recorders/ggogit/domain/member/repository/SeedRepository.java
+++ src/main/java/Recorders/ggogit/domain/member/repository/SeedRepository.java
@@ -3,7 +3,20 @@ package Recorders.ggogit.domain.member.repository;
 import Recorders.ggogit.domain.member.entity.Seed;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface SeedRepository {
+
     void save(Seed seed);
+
+    void update(Seed seed);
+
+    void delete(Seed seed);
+
+    List<Seed> findAll();
+
+    Seed findById(Long id);
+
+    Seed findByName(String name);
 }

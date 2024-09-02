ae3c4db19bb6fe46fd136bee0515ffef1cce4946
JJ <jyj961004@gmail.com>
2024-09-02 18:30:13 +0900

    commit feature/service-member

--- src/main/java/Recorders/ggogit/domain/member/service/MemberService.java
+++ src/main/java/Recorders/ggogit/domain/member/service/MemberService.java
@@ -1,10 +1,13 @@
 package Recorders.ggogit.domain.member.service;
 
+import Recorders.ggogit.domain.member.entity.Member;
+
 public interface MemberService {
 
-    /*
-    *
+    /**
+    * 회원가입
+    * @param member 회원 entity
     */
-
+    void registerMember(Member member);
 
 }

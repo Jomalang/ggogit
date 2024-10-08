cb27cfb82757b6638a38743fc549b24b4540489a
JJ <jyj961004@gmail.com>
2024-10-08 18:30:29 +0900

    Add: #126 [화면] 헤더 컴포넌트 (진행중)

---
+++ frontend/src/components/header/Header.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <!-- header-search-link, /tree/book/select, /svg/lens.svg -->
+    <div class="header-search-link">
+        <a class="header-search-link__link" href="">
+            <div class="header-search-link__input-box">
+                <p class="header-search-link__placeholder">나의 트리 검색</p>
+                <img class="header-search-link__icon" src="" alt="lens.svg"/>
+            </div>
+        </a>
+    </div>
+</template>
+
+<style scoped>
+
+</style>

c77bdd98605a305685cd7be977f57bfbd3501249
Jomalang <rhkddlr98@naver.com>
2024-10-10 01:33:30 +0900

    마이그레이션시작

---
+++ frontend/src/components/input/InputSelectBox.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="input-select-box">
+    <label class="input-select__subject">
+      <span>{{ label:string }}</span>
+      <div class="input-select--select__selected">
+        <p class="input-select--select__selected-description">선택해주세요</p>
+        <button class="input-next-button" :id="id" type="button">
+          <img src="@/svg/next.svg" alt="next-button" />
+        </button>
+      </div>
+    </label>
+  </div>
+</template>
+
+<style scoped></style>

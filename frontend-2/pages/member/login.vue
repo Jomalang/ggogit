2a012a80f75dcfac2c2fa8ec698bdb758b54b908
JJ <jyj961004@gmail.com>
2024-10-28 18:26:20 +0900

    [기능] 회원 정보 조회 (진행중)

--- frontend-2/pages/member/login.vue
+++ frontend-2/pages/member/login.vue
@@ -22,11 +22,6 @@
 </template>
 
 <style scoped>
-@import url("/src/assets/css/common/effect.css");
-@import url("/src/assets/css/common/reset.css");
-@import url("/src/assets/css/common/text.css");
-@import url("/src/assets/css/common/variables.css");
-@import url("/src/assets/css/layout.css");
 
 .login-member__join-page-container {
   width: auto;

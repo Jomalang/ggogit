66627656f6c1b4d0151ed0cdd162d4bdfb879b39
JJ <jyj961004@gmail.com>
2024-10-30 15:55:06 +0900

    [기능] 회원 정보 조회 (완료)

--- frontend-2/pages/member/new.vue
+++ frontend-2/pages/member/new.vue
@@ -29,7 +29,7 @@
         name="introduction"
         placeholder="간단한 한 줄 소개를 작성해 주세요"
     />
-    <CheckboxJoinPolicyAgreement label="이용약관 동의" />
+    <CheckboxJoinPolicyAgreement label="이용약관 동의"/>
     <SubmitBtnFullBar text="회원가입" />
   </div>
 </template>

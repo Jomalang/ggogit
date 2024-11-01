545f5b1c5b4b2f11191721ffb572dee082d6256c
TaegyuHan <gksxorb147@gmail.com>
2024-11-01 17:23:47 +0900

    Add: #209 도서 Tree 생성 유효성 검사 적용

--- frontend-2/components/input/TextBox.vue
+++ frontend-2/components/input/TextBox.vue
@@ -1,27 +1,38 @@
 <script setup>
 
-const props = defineProps({
-  label: String,
-  name: String,
-  placeholder: String,
-  modelValue: String
-});
+const { data } = defineProps(["data"]);
 
-const emit = defineEmits(['update:modelValue']);
+// const props = defineProps({
+//   label: String,
+//   name: String,
+//   placeholder: String,
+//   modelValue: String,
+//   validate: Boolean,
+//   validateMessage: {
+//     type: String,
+//     default: '올바른 형식이 아닙니다.'
+//   }
+// });
+
+const emit = defineEmits(['inputData']);
 
 </script>
 
 <template>
   <div class="input-text__bar">
     <label class="input-text__label">
-      <span class="input-text__label-text">{{ label }}</span>
+      <span class="input-text__label-text">{{ data.label }}</span>
       <input class="input-text__input"
-             :name="name"
-             :placeholder="placeholder"
-             :value="modelValue"
-             @input="$emit('update:modelValue', $event.target.value)"
+             :name="data.name"
+             :placeholder="data.placeholder"
+             :value="data.value"
+             :class="{ 'input-text__input--warning': data.validate }"
+             @input="emit('inputData', $event.target.value)"
              autocomplete="off"/>
     </label>
+    <div v-if="data.validate" class="input-text__wrong-box">
+      <p class="input-text__wrong-text">{{ data.validateMessage }}</p>
+    </div>
   </div>
 </template>
 
@@ -71,5 +82,12 @@ const emit = defineEmits(['update:modelValue']);
 .input-text__input:read-only{
   background-color: var(--main2, #e5eddb);
   outline: 3px solid var(--gray);
+}
+
+.input-text__wrong-box {
+  margin: 12px 10px;
+  p {
+    color: var(--warning, #ba0c0c);
+  }
 }
 </style>

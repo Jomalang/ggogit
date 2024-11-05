5aef7afcdbccdf5bf9555ba79b242e50bbbef10c
Gun-code <dlgmlrnjs213@gmail.com>
2024-11-05 18:34:36 +0900

    Add: Tree Search 페이지 구현

---
+++ frontend-2/components/input/InputBackSearchTree.vue
@@ -0,0 +1,248 @@
+<script setup>
+import { ref } from "vue";
+//-----------------props-----------------
+const props = defineProps({
+  placeholder: "",
+  href: "",
+  api: "",
+  page: 1,
+});
+//-----------------emit-----------------
+const emit = defineEmits([
+  "req",
+  "bookResult",
+  "page",
+  "totalCount",
+  "totalPage",
+]);
+//-----------------ref-----------------
+const bookResult = ref([]);
+const query = ref("");
+const filter = ref("title");
+const page = ref(1);
+const totalCount = ref(0);
+const totalPage = ref(0);
+
+watch(page, () => {
+  if (page.value > 1) {
+    createReq(query.value, filter.value, page.value);
+    console.log("new page");
+  }
+});
+
+const createReq = async (query, filter, currentPage) => {
+  try {
+    const response = await $fetch(props.api, {
+      method: "GET",
+      params: {
+        q: query,
+        f: filter,
+        p: currentPage,
+      },
+    });
+    if (response !== undefined) {
+      if (currentPage === 1) {
+        bookResult.value = response.books;
+      } else {
+        bookResult.value = [...bookResult.value, ...response.books];
+      }
+      page.value = currentPage;
+      totalCount.value = response.totalCount;
+      totalPage.value = response.totalPage;
+    } else {
+      bookResult.value = [];
+      totalCount.value = 0;
+      totalPage.value = 1;
+    }
+    emit("bookResult", bookResult.value);
+    emit("req", query);
+    emit("page", page.value);
+    emit("totalCount", totalCount.value);
+    emit("totalPage", totalPage.value);
+  } catch (error) {
+    if (error.response && error.response.status === 400) {
+      alert("검색어를 두 글자 이상 입력해 주세요.");
+    } else {
+      alert("오류가 발생했습니다. 다시 시도해 주세요.");
+    }
+  }
+};
+
+//-----------------lifeCycle-----------------
+onUpdated(() => {
+  if (page.value < props.page) {
+    page.value = props.page;
+  }
+});
+</script>
+
+<template>
+  <!-- input-back-search(placeholder, href, method, name) -->
+  <div class="search__form">
+    <div>
+      <a :href="props.href">
+        <img src="/public/svg/back.svg" alt="back button" />
+      </a>
+    </div>
+    <div class="search-bar">
+      <label class="search-bar--label">
+        <input
+          class="search-bar--input"
+          type="text"
+          :placeholder="props.placeholder"
+          v-model="query"
+          autocomplete="off"
+          @keyup.enter="createReq(query, filter, 1)"
+        />
+        <button class="search-bar--close" type="reset">
+          <img src="/public/svg/close-button.svg" alt="close-btn" />
+        </button>
+      </label>
+      <button @click="createReq(query, filter, 1)">
+        <img src="/public/svg/lens.svg" alt="lens" />
+      </button>
+    </div>
+  </div>
+
+  <div class="search-filter-log">
+    <label class="search-filter-log__checkbox-labal">
+      <input
+        class="search-filter-log__checkbox-input"
+        type="radio"
+        name="filterType"
+        value="title"
+        checked
+        v-model="filter"
+      />
+      <span class="search-filter-log__checkbox-input-text">제목</span>
+    </label>
+    <label class="search-filter-log__checkbox-labal">
+      <input
+        class="search-filter-log__checkbox-input"
+        type="radio"
+        name="filterType"
+        value="author"
+        v-model="filter"
+      />
+      <span class="search-filter-log__checkbox-input-text">저자</span>
+    </label>
+    <label class="search-filter-log__checkbox-labal">
+      <input
+        class="search-filter-log__checkbox-input"
+        type="radio"
+        name="filterType"
+        value="publisher"
+        v-model="filter"
+      />
+      <span class="search-filter-log__checkbox-input-text">출판사</span>
+    </label>
+  </div>
+</template>
+
+<style scoped>
+/* =================================
+      input-back-search, input-search 
+      검색버튼 뒤로가기버튼 있음. 없음.
+   ===================================
+ */
+a {
+  text-decoration: none;
+}
+
+button {
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+
+.search__form {
+  display: flex;
+  align-items: center;
+}
+
+.search-bar {
+  background: var(--main2);
+  border-radius: 20px;
+  padding: 8px 16px 8px 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-grow: 1;
+}
+
+.search-bar--label {
+  display: flex;
+  flex-grow: 1;
+  font-size: 14px;
+}
+
+.search-bar--input {
+  width: 100%;
+  font-weight: var(--semi-bold);
+  font-size: 14px;
+  background-color: var(--main2);
+  border: none;
+}
+
+.search-bar--input:focus {
+  outline: none;
+}
+
+/*  */
+.search-bar--close {
+  display: none;
+}
+
+.search-bar--input:not(:placeholder-shown) {
+  font-weight: var(--semi-bold);
+  font-size: 14px;
+  background-color: var(--main2);
+}
+
+/*  */
+.search-bar--input:not(:placeholder-shown) + .search-bar--close {
+  display: inline;
+  height: 18px;
+}
+
+/*  */
+.search-bar-img {
+  height: 18px;
+}
+/* 필터 */
+.search-filter-log {
+  margin-top: 18px;
+  display: flex;
+  white-space: nowrap;
+  scrollbar-width: none;
+  gap: 10px;
+}
+.search-filter {
+  display: flex;
+  justify-content: flex-start;
+  gap: 10px;
+}
+
+.search-filter-log__checkbox-input {
+  display: none;
+}
+
+.search-filter-log__checkbox-input-text {
+  font-family: "Pretendard", serif;
+  font-size: 12px;
+  font-weight: var(--medium, 500);
+  color: var(--text-sub, #767676);
+  border-radius: 8px;
+  background-color: #f7f7f7;
+  padding: 12px 20px;
+  cursor: pointer;
+  user-select: none;
+  flex-shrink: 0;
+}
+
+.search-filter-log__checkbox-input:checked
+  + .search-filter-log__checkbox-input-text {
+  background-color: var(--main1, #323a27);
+  color: var(--white, #ffffff);
+}
+</style>
--- frontend-2/pages/tree/search.vue
+++ frontend-2/pages/tree/search.vue
@@ -1,11 +1,151 @@
 <script setup lang="ts">
+import { reactive, ref } from "vue";
+import InputBackSearchTree from "~/components/input/InputBackSearchTree.vue";
 
+//---------------variable----------------
+const apiUrl = `${import.meta.env.VITE_API_BASE_URL}`;
+const keyword = ref("");
+const books = ref([]);
+const page = ref(1);
+const totalPage = ref(0);
+const totalCount = ref(0);
+const limit = ref(10);
+const scrollContainer = ref(null);
+//-------------handler----------------
+
+const handleTreeResult = (data) => {
+  books.value = data || [];
+  console.log(`length=${books.value.length}`);
+};
+
+const handleKeyword = (query) => {
+  keyword.value = query;
+  console.log(`keyword=${keyword.value}`);
+};
+
+const handlePage = (p) => {
+  page.value = p;
+  console.log(`page=${page.value}`);
+};
+
+const handleTotalPage = (totalP) => {
+  totalPage.value = totalP;
+  console.log(`totalPages=${totalPage.value}`);
+};
+
+const handleTotalCount = (totalC) => {
+  totalCount.value = totalC;
+  console.log(`totalCount=${totalCount.value}`);
+};
+
+const handleScroll = () => {
+  const container = scrollContainer.value;
+  const { scrollTop, clientHeight, scrollHeight } = container;
+  if (scrollTop + clientHeight >= scrollHeight) {
+    console.log("scroll end");
+    //최대 페이지까지만 증가
+    if (parseInt(page.value) < parseInt(totalPage.value)) {
+      page.value = parseInt(page.value) + 1;
+      console.log(`page=${page.value}`);
+    }
+  }
+};
+
+//-------------life cycle----------------
+onMounted(() => {
+  watch(
+      [scrollContainer],
+      () => {
+        if (scrollContainer.value) {
+          scrollContainer.value.addEventListener("scroll", handleScroll, {
+            passive: false,
+          });
+        }
+      },
+      { immediate: true }
+  );
+});
 </script>
 
 <template>
+  <header class="reg-book-search-container">
+    <h1 class="none">트리 검색</h1>
+    <section>
+      <h2 class="none">트리 검색 창</h2>
+      <InputBackSearchTree
+          :placeholder="`검색할 트리를 입력해주세요.`"
+          :href="`./seed/index`"
+          :api="apiUrl"
+          :page="page"
+          @treeResult="handleTreeResult"
+          @req="handleKeyword"
+          @page="handlePage"
+          @totalCount="handleTotalCount"
+          @totalPage="handleTotalPage"
+      />
+    </section>
 
+    <section>
+      <h2 class="none">검색 결과 개수 및 최근 수정한 순서</h2>
+      <TopBarSearchResultNum :num="totalCount" />
+    </section>
+  </header>
+
+  <main>
+    <section v-if="keyword === ''">
+      <div class="text-info-container">
+        <h2 class="none">검색 시작 안내</h2>
+        <TextInfo
+            :text="'현재 검색중인 트리가 없습니다'"
+            :boldText="'트리를 검색하거나 직접 등록해주세요'"
+        />
+      </div>
+    </section>
+
+    <section class="tree-card-list" v-else-if="totalCount >= 1">
+      <h3 class="none">트리 검색 결과</h3>
+      <div class="scroll-container" ref="scrollContainer">
+        <div v-for="tree in trees" :key="tree.id">
+          <CardBookPreviews :data="tree" />
+        </div>
+      </div>
+    </section>
+
+    <section v-else="totalCount === 0">
+      <h3 class="none">검색 결과 없음</h3>
+      <div class="text-info-container">
+        <TextInfo
+            :text="'검색 결과가 없습니다.'"
+            :boldText="'다시 검색하거나 직접 등록해주세요'"
+        />
+      </div>
+    </section>
+  </main>
+
+  <section class="btn-select-container--right">
+    <h2 class="none">트리 직접 등록 버튼</h2>
+    <!-- TODO: href변경하기 -->
+    <ButtonBtnShortAGreen
+        :link="`/tree/seed`"
+        :text="`트리 직접 등록하기`"
+    />
+  </section>
+
+  <div class="nav-back-container">
+    <h2 class="none">네비게이션 뒤 공백</h2>
+  </div>
+
+  <aside class="nav-container">
+    <NavNavigationBar active="home" />
+  </aside>
 </template>
 
 <style scoped>
-
+.scroll-container {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  overflow-y: auto;
+  height: 480px;
+}
 </style>

37e86221d7d8e930ed7e243d89b4f223ba4f4e1b
TaegyuHan <gksxorb147@gmail.com>
2024-11-13 18:21:28 +0900

    Add: #250 [기능] OAuth 구글 로그인 로직 진행중

--- frontend-2/components/link/SocialLogin.vue
+++ frontend-2/components/link/SocialLogin.vue
@@ -9,39 +9,41 @@ const router = useRouter();
 
 const googleLoginHandler = async () => {
   try {
-      let token;
-      let userInfo;
-      {
-        let response = await googleTokenLogin();
-        token = response.access_token;
-      }
-      userInfo = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${token}`);
-      userInfo = await userInfo.json();
-    {
-      const isMember = await $fetch(`${config.public.apiBase}/auth/isMember`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({email: userInfo.email})
-      });
-      if (isMember) {
-        const response = await $fetch(`${config.public.apiBase}/auth/newToken`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({email: userInfo.email})
-        });
-        memberDetail.setAuthWithToken(response.accessToken, response.refreshToken);
-        await router.push('/home');
-      } else {
-        console.log('userInfo', userInfo.email, userInfo.name, userInfo.picture);
-        joinInfo.setJoinInfo(userInfo);
-        console.log('joinInfo', joinInfo);
-        await router.push("/member/new");
-      }
+
+    const response = await googleTokenLogin();
+    const token = response.access_token;
+
+    let userInfo = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${token}`);
+    userInfo = await userInfo.json();
+
+    const isMember = await $fetch(`${config.public.apiBase}/auth/isMember`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email: userInfo.email})
+    });
+
+    if (!isMember) { // 회원이 아닌 경우
+      console.log('userInfo', userInfo.email, userInfo.name, userInfo.picture);
+      joinInfo.setJoinInfo(userInfo);
+      console.log('joinInfo', joinInfo);
+      await router.push("/member/oauth/new");
+      return;
     }
+
+    // 회원인 경우
+    const newTokenResponse = await $fetch(`${config.public.apiBase}/auth/newToken`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email: userInfo.email})
+    });
+
+    memberDetail.setAuthWithToken(newTokenResponse.accessToken);
+    await router.push('/home');
+
   } catch (error) {
     console.error('Google 로그인 중 오류 발생:', error);
   }
--- frontend-2/composables/useMemberJoinTmp.js
+++ frontend-2/composables/useMemberJoinTmp.js
@@ -6,12 +6,39 @@ export const useMemberJoinTmp = defineStore("ggogitMemberJoinTmp", () => {
     const _profile = ref("");
 
     function setJoinInfo(joinInfo) {
-        console.log('joinInfo', joinInfo.email, joinInfo.name, joinInfo.picture);
+
+        _username.value = joinInfo.name;
+        _email.value = joinInfo.email;
+        _profile.value = joinInfo.picture;
+
         localStorage.setItem("_username",joinInfo.name);
         localStorage.setItem("_email",joinInfo.email);
         localStorage.setItem("_profile",joinInfo.picture);
     }
+
+    function getJoinInfo() {
+
+        if (_username.value === "" || _email.value === "" || _profile.value === "") {
+            if (import.meta.env.SSR) { return; }
+            try {
+                _username.value = localStorage.getItem("_username");
+                _email.value = localStorage.getItem("_email");
+                _profile.value = localStorage.getItem("_profile");
+            } catch (e) {
+                console.log(e);
+                return;
+            }
+        }
+
+        return {
+            name: _username.value,
+            email: _email.value,
+            picture: _profile.value
+        };
+    }
+
     return {
-        setJoinInfo
+        setJoinInfo,
+        getJoinInfo
     };
 });

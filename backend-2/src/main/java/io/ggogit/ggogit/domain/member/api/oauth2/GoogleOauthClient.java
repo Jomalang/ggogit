30d7fba6443bda9f34b496cf3d714595099c42de
Gun-code <dlgmlrnjs213@gmail.com>
2024-11-13 18:15:53 +0900

    OAuth2 변경 작업

---
+++ backend-2/src/main/java/io/ggogit/ggogit/domain/member/api/oauth2/GoogleOauthClient.java
@@ -0,0 +1,14 @@
+package io.ggogit.ggogit.domain.member.api.oauth2;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class GoogleOauthClient {
+
+    public String
+
+}
--- frontend-2/components/link/SocialLogin.vue
+++ frontend-2/components/link/SocialLogin.vue
@@ -9,39 +9,42 @@ const router = useRouter();
 
 const googleLoginHandler = async () => {
   try {
-      let token;
-      let userInfo;
-      {
-        let response = await googleTokenLogin();
-        token = response.access_token;
-      }
-      userInfo = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${token}`);
-      userInfo = await userInfo.json();
-    {
-      const isMember = await $fetch(`${config.public.apiBase}/auth/isMember`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({email: userInfo.email})
-      });
-      if (isMember) {
-        const response = await $fetch(`${config.public.apiBase}/auth/newToken`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({email: userInfo.email})
-        });
-        memberDetail.setAuthWithToken(response.accessToken, response.refreshToken);
-        await router.push('/home');
-      } else {
-        console.log('userInfo', userInfo.email, userInfo.name, userInfo.picture);
-        joinInfo.setJoinInfo(userInfo);
-        console.log('joinInfo', joinInfo);
-        await router.push("/member/new");
-      }
+
+    const response = await googleTokenLogin();
+    const token = response.access_token;
+
+    let userInfo = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${token}`);
+    console.log(userInfo)
+    userInfo = await userInfo.json();
+
+    const isMember = await $fetch(`${config.public.apiBase}/auth/isMember`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email: userInfo.email})
+    });
+
+    if (!isMember) { // 회원이 아닌 경우
+      console.log('userInfo', userInfo.email, userInfo.name, userInfo.picture);
+      joinInfo.setJoinInfo(userInfo);
+      console.log('joinInfo', joinInfo);
+      await router.push("/member/oauth/new");
+      return;
     }
+
+    // 회원인 경우
+    const newTokenResponse = await $fetch(`${config.public.apiBase}/auth/newToken`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email: userInfo.email})
+    });
+
+    memberDetail.setAuthWithToken(newTokenResponse.accessToken);
+    await router.push('/home');
+
   } catch (error) {
     console.error('Google 로그인 중 오류 발생:', error);
   }
@@ -53,16 +56,20 @@ const googleLoginHandler = async () => {
   <div class="social-login">
     <div class="social-login__icons">
       <a @click.prevent="googleLoginHandler">
-        <img src="/public/svg/google-circle.svg" alt="`구글 로그인`" />
+        <img src="/public/svg/google-circle.svg" alt="`구글 로그인`"/>
       </a>
       <a href="#"
-        ><div>
-          <img src="/public/svg/naver-circle.svg" alt="`네이버 로그인`" /></div
-      ></a>
+      >
+        <div>
+          <img src="/public/svg/naver-circle.svg" alt="`네이버 로그인`"/></div
+        >
+      </a>
       <a href="#"
-        ><div>
-          <img src="/public/svg/kakao-circle.svg" alt="`카카오 로그인`" /></div
-      ></a>
+      >
+        <div>
+          <img src="/public/svg/kakao-circle.svg" alt="`카카오 로그인`"/></div
+        >
+      </a>
     </div>
   </div>
 </template>

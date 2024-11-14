57741b6b8d16c3caf8682b7cc073439413a89398
TaegyuHan <gksxorb147@gmail.com>
2024-11-14 17:52:56 +0900

    Add: #264 `useGetImageUrl` 기본 URL 적용

--- frontend-2/pages/member/login.vue
+++ frontend-2/pages/member/login.vue
@@ -34,6 +34,7 @@ const loginApi = async () => {
 
     memberDetail.setAuthWithToken(response.accessToken);
     //현재 URL에서 returnUrl 값을 가져오기
+    // console.log('리턴 URL 확인', router.currentRoute.value.query.retrunUrl);
     const returnUrl = router.currentRoute.value.query.retrunUrl || "/home";
     console.log("로그인 성공");
     // 로그인 성공시 returnUrl로 이동

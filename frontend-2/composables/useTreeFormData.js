fed686d35e60cfb5a04627b115d939320c1a1dfe
Taegyu <59700057+TaegyuHan@users.noreply.github.com>
2024-11-16 12:49:16 +0900

    Add: #277 [버그] 트리 생성 컴포저블로 변경

---
+++ frontend-2/composables/useTreeFormData.js
@@ -0,0 +1,57 @@
+const _treeFormData = ref({
+    // 트리 씨앗 정보
+    seedId: 1,
+
+    // 도서 정보
+    bookTitle: "",
+    bookTitleValid: true,
+
+    // 지은이 정보
+    author: "",
+    authorValid: true,
+
+    // 출판일 정보
+    publishDate: "",
+    publishDateValid: true,
+
+    // 출판사 정보
+    publisher: "",
+    publisherValid: true,
+
+    // 총 페이지 정보
+    totalPage: "",
+    totalPageValid: true,
+
+    // 도서 카테고리 정보
+    bookCategoryId: null,
+    bookCategoryIdValid: true,
+
+    // 도서 카테고리 이름 정보
+    bookCategoryName: null,
+    bookCategorySelected: true,
+
+    // 트리 정보
+    treeTitle: "",
+    treeTitleValid: true,
+
+    // 트리 설명 정보
+    description: "",
+    descriptionValid: true,
+
+    // 공개 여부 정보
+    visibility: false,
+    visibilityValid: true,
+
+    // 이미지 정보
+    imageData: "",
+
+    // 트리 생성 경로
+    createUrl: `/tree/book/new`,
+});
+
+export default function useTreeFormData() {
+
+    return {
+        treeFormData: _treeFormData,
+    }
+}
